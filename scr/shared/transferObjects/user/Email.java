9395352240e984dac2b295759b8c1b34764a105e
vlad151 <lazar.vlad151@gmail.com>
2021-12-01 15:31:00 +0100

    email and password classes with validation method for both

--- scr/shared/transferObjects/user/Email.java
+++ scr/shared/transferObjects/user/Email.java
@@ -1,11 +1,23 @@
 package shared.transferObjects.user;
 
-public class Email
+import java.io.Serializable;
+import java.util.regex.Pattern;
+
+public class Email implements Serializable
 {
   private String email;
 
   public Email(String email)
   {
     this.email = email;
+  }
+
+  public boolean isValid()
+  {
+    String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@"
+        + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+
+    Pattern pat = Pattern.compile(emailRegex);
+    return pat.matcher(email).matches();
   }
 }
